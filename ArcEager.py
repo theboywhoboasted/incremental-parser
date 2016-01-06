64084c00a9988b876e63f84fb025470adfc7bef5
arpit <kooldude.arpit@gmail.com>
2016-01-06 19:48:38 +0530

    Arc eager updated

--- ArcEager.py
+++ ArcEager.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 
 class NonProjectiveParseError(Exception):
-	""" To be raised if the sentence has a non-projective dependency
-		since Arc Eager can only process projective dependencies"""
+	""" To be raised, when the sentence contains a NonProjective dependency 
+	since ArcEager can only parse projective dependencies """
 	def __init__(self, arg):
 		self.arg = arg
 	def __str__(self):
 		return repr(self.arg)
 
 class ArcEagerState:
+
 
 	def __init__(self, sentence, func):
 		""" initializes the state of the parser 
@@ -20,11 +21,10 @@ class ArcEagerState:
 		log contains the list of (transition, feature set) (initially empty)
 		transitions contains the list of transitions (initially empty)
 		likelihood is the probability of parse initialized to 1
-		gold_trans is the number of times when the correct arc-shift was available
-		gold_trans is the number of times when the correct arc-shift was available with the right label
+		gold_trans and gold_label contain the number of times the gold arc could be made
+		# TODO: add support for gold_label, make the comments more descriptive!
 		get_state is the function that generates features from state 
 		"""
-
 		self.stack = [{'index': '0',
 						'form': 'ROOT',
 						'lemma': 'ROOT_lemma',
@@ -49,9 +49,11 @@ class ArcEagerState:
 		self.gold_trans = 0.0
 		self.gold_label = 0.0
 
+
 	# static variables
 	transition_types = [("SHIFT", None), ("REDUCE", None)]
 	transition_codes = {j:str(i) for (i,j) in enumerate(transition_types)}
+
 
 	def possible(self, transition):
 		""" indicates if a given transition is possible given the parser state """
@@ -67,9 +69,11 @@ class ArcEagerState:
 			return (self.parent[self.stack[-1]['index']] != None)
 			return False
 
+
 	def is_incomplete(self):
 		""" indicates if the parse is incomplete """
 		return (len(self.stack) > 1) or (self.index < len(self.buffer))
+
 
 	def next_transition_and_label(self):
 		""" given the parser state and dependency parse, returns the next transition """
@@ -96,15 +100,20 @@ class ArcEagerState:
 			# print "NPPE", [(word['index'], self.parent[word['index']]) for word in self.stack], self.index
 			raise NonProjectiveParseError(self.log)
 
+
 	def make_arc(self, arc, label):
+		""" make the appropriate arc and do the bookkeeping in arcs, parent, children and label """
 		(parent, child) = arc
 		self.arcs.append((parent['index'], child['index'], label))
 		self.parent[child['index']] = parent
 		self.children[parent['index']].append(child)
 		self.label[child['index']] = label
 
+
 	def make_transition(self, transition, label=None, prob=-1):
 		""" given a transition with a probability, makes the transition to the parser state """
+
+		# see if a gold arc arc can be made and update gold_trans
 		try:
 			if self.stack[-1]['parent'] == self.buffer[self.index]['index']:
 				self.gold_trans += 1
@@ -112,12 +121,21 @@ class ArcEagerState:
 				self.gold_trans += 1
 		except IndexError:
 			pass
+
+		# check if the transition asked for is actually possible
 		assert self.possible(transition)
-		self.log.append(((transition, label),self.get_state(self)))
+
+		# log the transition being made with the feature set if the model is being trained
+		if prob == -1:
+			self.log.append(((transition, label),self.get_state(self)))
+		
+		# if the labeled transition does not exist in the present set, add it
 		if label:
 			if (transition, label) not in ArcEagerState.transition_types:
 				ArcEagerState.transition_codes[(transition, label)] = str(len(ArcEagerState.transition_types))
 				ArcEagerState.transition_types.append((transition,label))
+
+		# make the actual transition
 		if transition == 'SHIFT':
 			self.stack.append(self.buffer[self.index])
 			self.index += 1
@@ -135,3 +153,19 @@ class ArcEagerState:
 		self.transitions.append(transition)
 		if prob > 0:
 			self.likelihood *= prob
+
+
+	@staticmethod
+	def recover_transitions(trans_file):
+		""" recovers the set of transitions from the trans_file """
+		transitions = []
+		with open(trans_file, 'r') as fr:
+			for line in fr.readlines():
+				words = line.strip().split(', ')
+				if (words[1].strip()[0] == "'") and (words[1].strip()[-1] == "'"):
+					transitions.append((words[0], words[1].split("'")[1]))
+				else:
+					transitions.append((words[0], None))
+		ArcEagerState.transition_types = transitions
+		ArcEagerState.transition_codes = {j:str(i) for (i,j) in enumerate(ArcEagerState.transition_types)}
+		# print "there are", len(transitions), "transitions", transitions
